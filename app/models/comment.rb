df8f92dd424f5f1c3ab2a1917ac9d97a1f297158
RyoSuzuki <sryo0722@gmail.com>
2021-01-07 20:45:42 +0900

    modify-validates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
 
 　validates :Content,{presence: true}
+  validates :user_id,{presence: true}
+  validates :post_id,{presence: true}
 
   belongs_to :user
   belongs_to :post
