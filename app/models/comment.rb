d23e0662f422597b3a51d0886d22217d834444e2
RyoSuzuki <sryo0722@gmail.com>
2021-01-19 17:45:57 +0900

    Alter-post-show-synthax

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
 
-　validates :Content,{presence: true}
+  validates :Content,{presence: true}
   validates :user_id,{presence: true}
   validates :post_id,{presence: true}
 
